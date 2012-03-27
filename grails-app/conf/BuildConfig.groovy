5241d482e363e8c8db3c7b57bfdcc4ed562032a7
Marc Palmer <marc@anyware.co.uk>
2012-03-27 21:51:33 +0100

    Updated to grails 2.0.1 and new platform core

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,6 +7,7 @@ grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
+        excludes 'xerces', 'xml-apis'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
